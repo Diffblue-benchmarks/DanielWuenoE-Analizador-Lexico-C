ec047583d1801dc63ee2720b4f2ee831d87727df
jmartinixta <jmartinixta@gmail.com>
2019-05-22 22:57:16 -0500

    q00

--- src/analizador/lexico/c/Clasifica.java
+++ src/analizador/lexico/c/Clasifica.java
@@ -1,5 +1,26 @@
 package analizador.lexico.c;
 
 public class Clasifica {
-    ///
+    
+    private String caso;
+    Tipos tipo = new Tipos();
+    ConversionCaracter conv = new ConversionCaracter();
+
+    public void q0(String archivo) {
+        for (int i = 0; i < archivo.length(); i++) {
+            conv.convertirCaracter(archivo.charAt(i));
+
+            if (tipo.esCero(conv.getAscii()) == true) {
+                caso = "q1";
+            } else if (tipo.esNumero(conv.getAscii()) == true) {
+                caso = "q3";
+            } else if (tipo.esMinuscula(conv.getAscii()) == true || tipo.esMayuscula(conv.getAscii()) == true) {
+                caso = "q4";
+            } else if (tipo.esCaracter(conv.getAscii()) == true) {
+                caso = "q7";
+            } else {
+                caso = "q2";
+            }
+        }a
+    }
 }
